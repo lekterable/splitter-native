110310b6e78eba3fb6de24a0cb6bc2d1124e400f
Kornel Dubieniecki <lekterable@gmail.com>
2019-02-26 23:53:55 +0100

    Fetch data from the backend

--- App.js
+++ App.js
@@ -1,4 +1,17 @@
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
 import Root from './screens'
 
-export default () => <Root />
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000/' }),
+  cache: new InMemoryCache()
+})
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Root />
+  </ApolloProvider>
+)
--- screens/app/HomeScreen.js
+++ screens/app/HomeScreen.js
@@ -1,6 +1,17 @@
+import gql from 'graphql-tag'
 import React from 'react'
+import { Query } from 'react-apollo'
 import { Button, Text, View } from 'react-native'
 import { handleSignOut } from '../../utils/auth'
+
+const GET_EXPENSES_QUERY = gql`
+  {
+    expenses(household: "1") {
+      id
+      type
+    }
+  }
+`
 
 export default ({ navigation }) => {
   const onSignOut = () =>
@@ -8,7 +19,20 @@ export default ({ navigation }) => {
 
   return (
     <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-      <Text>Home</Text>
+      <Query query={GET_EXPENSES_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <Text>'Loading...'</Text>
+          if (error) return <Text>${`Error! ${error.message}`}</Text>
+
+          return (
+            <View>
+              {data.expenses.map(({ id, type }, i) => (
+                <Text key={id}>{`${i + 1}. ${type} `}</Text>
+              ))}
+            </View>
+          )
+        }}
+      </Query>
       <Button title="Sign Out" onPress={onSignOut} />
     </View>
   )
