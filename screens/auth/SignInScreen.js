0960baeb963043c2a007ead3c6f8b99dd85f130b
Kornel Dubieniecki <lekterable@gmail.com>
2019-03-04 23:39:08 +0100

    Add sign in form

--- screens/auth/SignInScreen.js
+++ screens/auth/SignInScreen.js
@@ -1,41 +1,84 @@
 import gql from 'graphql-tag'
-import React from 'react'
+import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
-import { Button, Text, View } from 'react-native'
+import { Button, TextInput, View } from 'react-native'
 import { handleSignIn } from '../../utils/auth'
 
-export const LOGIN_USER_QUERY = gql`
-  mutation {
-    login(email: "joey@gmail.com", password: "joey123")
+const LOGIN_USER_QUERY = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password)
   }
 `
+export default class extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
 
-export default ({ navigation }) => {
-  const onSignIn = token =>
-    handleSignIn(token).then(() => navigation.navigate('InitScreen'))
+  onSignIn = token =>
+    handleSignIn(token).then(() => this.props.navigation.navigate('InitScreen'))
 
-  return (
-    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-      <Mutation mutation={LOGIN_USER_QUERY}>
-        {login => (
-          <View
-            style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}
-          >
-            <Text>Sign In</Text>
-            <Button
-              title="Sign In"
-              onPress={async () => {
-                const { data } = await login()
-                onSignIn(data.login)
+  render() {
+    const { email, password } = this.state
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Mutation mutation={LOGIN_USER_QUERY}>
+          {login => (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                flex: 1
               }}
-            />
-            <Button
-              title="Sign Up"
-              onPress={() => navigation.navigate('SignUp')}
-            />
-          </View>
-        )}
-      </Mutation>
-    </View>
-  )
+            >
+              <TextInput
+                style={{
+                  height: 30,
+                  borderColor: 'gray',
+                  width: 150,
+                  borderWidth: 1,
+                  textAlign: 'center',
+                  borderRadius: 10
+                }}
+                placeholder="email"
+                autoCapitalize="none"
+                autoComplete="off"
+                onChangeText={text => this.setState({ email: text })}
+                value={email}
+              />
+              <TextInput
+                style={{
+                  height: 30,
+                  borderColor: 'gray',
+                  width: 150,
+                  borderWidth: 1,
+                  textAlign: 'center',
+                  borderRadius: 10
+                }}
+                placeholder="password"
+                autoCapitalize="none"
+                autoComplete="off"
+                secureTextEntry
+                onChangeText={text => this.setState({ password: text })}
+                value={password}
+              />
+              <Button
+                title="Sign In"
+                onPress={async () => {
+                  const { data } = await login({
+                    variables: { email, password }
+                  })
+                  this.onSignIn(data.login)
+                }}
+              />
+              <Button
+                title="Sign Up"
+                onPress={() => this.props.navigation.navigate('SignUp')}
+              />
+            </View>
+          )}
+        </Mutation>
+      </View>
+    )
+  }
 }
