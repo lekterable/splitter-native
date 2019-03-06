59ced34e2198c53193d44b3dccb317007cdced2e
Kornel Dubieniecki <lekterable@gmail.com>
2019-03-06 09:43:12 +0100

    Fetch user households

--- screens/app/HomeScreen.js
+++ screens/app/HomeScreen.js
@@ -4,11 +4,11 @@ import { Query } from 'react-apollo'
 import { Button, Text, View } from 'react-native'
 import { handleSignOut } from '../../utils/auth'
 
-const GET_EXPENSES_QUERY = gql`
+const GET_HOUSEHOLDS_QUERY = gql`
   {
-    expenses(household: "1") {
+    households {
       id
-      type
+      name
     }
   }
 `
@@ -19,15 +19,15 @@ export default ({ navigation }) => {
 
   return (
     <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-      <Query query={GET_EXPENSES_QUERY}>
+      <Query query={GET_HOUSEHOLDS_QUERY}>
         {({ loading, error, data }) => {
           if (loading) return <Text>'Loading...'</Text>
           if (error) return <Text>${`Error! ${error.message}`}</Text>
 
           return (
             <View>
-              {data.expenses.map(({ id, type }, i) => (
-                <Text key={id}>{`${i + 1}. ${type} `}</Text>
+              {data.households.map(({ id, name }, i) => (
+                <Text key={id}>{`${i + 1}. ${name} `}</Text>
               ))}
             </View>
           )
