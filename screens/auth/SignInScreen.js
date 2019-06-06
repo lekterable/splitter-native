8fd5f9570646f22c008ace4a8321c4398317cd0d
Kornel Dubieniecki <lekterable@gmail.com>
2019-06-07 00:14:03 +0200

    Refactor SignIn form using hooks :tada:

--- screens/auth/SignInScreen.js
+++ screens/auth/SignInScreen.js
@@ -1,83 +1,78 @@
-import gql from 'graphql-tag';
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
-import { Button, TextInput, View } from 'react-native';
-import auth from '../../utils/auth';
+import gql from 'graphql-tag'
+import React, { useState } from 'react'
+import { Mutation } from 'react-apollo'
+import { Button, TextInput, View } from 'react-native'
+import auth from '../../utils/auth'
 
 const LOGIN_USER_QUERY = gql`
   mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password)
   }
 `
-export default class extends Component {
-  state = {
-    email: '',
-    password: ''
-  }
 
-  render() {
-    const { email, password } = this.state
-    return (
-      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-        <Mutation mutation={LOGIN_USER_QUERY}>
-          {(login, { client }) => (
-            <View
+export default ({ navigation }) => {
+  const [form, setForm] = useState({ email: '', password: '' })
+
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      <Mutation mutation={LOGIN_USER_QUERY}>
+        {(login, { client }) => (
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              flex: 1
+            }}
+          >
+            <TextInput
               style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                flex: 1
+                height: 30,
+                borderColor: 'gray',
+                width: 150,
+                borderWidth: 1,
+                textAlign: 'center',
+                borderRadius: 10
               }}
-            >
-              <TextInput
-                style={{
-                  height: 30,
-                  borderColor: 'gray',
-                  width: 150,
-                  borderWidth: 1,
-                  textAlign: 'center',
-                  borderRadius: 10
-                }}
-                placeholder="email"
-                autoCapitalize="none"
-                autoComplete="off"
-                onChangeText={text => this.setState({ email: text })}
-                value={email}
-              />
-              <TextInput
-                style={{
-                  height: 30,
-                  borderColor: 'gray',
-                  width: 150,
-                  borderWidth: 1,
-                  textAlign: 'center',
-                  borderRadius: 10
-                }}
-                placeholder="password"
-                autoCapitalize="none"
-                autoComplete="off"
-                secureTextEntry
-                onChangeText={text => this.setState({ password: text })}
-                value={password}
-              />
-              <Button
-                title="Sign In"
-                onPress={async () => {
-                  const { data } = await login({
-                    variables: { email, password }
-                  })
-                  await auth.authenticate(data.login)
-                  await client.resetStore()
-                  this.props.navigation.navigate('InitScreen')
-                }}
-              />
-              <Button
-                title="Sign Up"
-                onPress={() => this.props.navigation.navigate('SignUp')}
-              />
-            </View>
-          )}
-        </Mutation>
-      </View>
-    )
-  }
+              placeholder="email"
+              autoCapitalize="none"
+              autoComplete="off"
+              onChangeText={email => setForm({ ...form, email })}
+              value={form.email}
+            />
+            <TextInput
+              style={{
+                height: 30,
+                borderColor: 'gray',
+                width: 150,
+                borderWidth: 1,
+                textAlign: 'center',
+                borderRadius: 10
+              }}
+              placeholder="password"
+              autoCapitalize="none"
+              autoComplete="off"
+              secureTextEntry
+              onChangeText={password => setForm({ ...form, password })}
+              value={form.password}
+            />
+            <Button
+              title="Sign In"
+              onPress={async () => {
+                const { data } = await login({
+                  variables: { ...form }
+                })
+                await auth.authenticate(data.login)
+                await client.resetStore()
+                navigation.navigate('InitScreen')
+              }}
+            />
+            <Button
+              title="Sign Up"
+              onPress={() => navigation.navigate('SignUp')}
+            />
+          </View>
+        )}
+      </Mutation>
+    </View>
+  )
 }
