37795b8ed5c3338572908d026fc18d9ca3ec7598
Kornel Dubieniecki <lekterable@gmail.com>
2020-03-28 18:35:44 +0100

    Add QRCode scanner

--- src/components/Other/index.js
+++ src/components/Other/index.js
@@ -1,10 +1,48 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import React, { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
-const Other = () => (
-  <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-    <Text>Other</Text>
-  </View>
-)
+const Other = () => {
+  const [hasPermission, setHasPermission] = useState(null)
+  const [code, setCode] = useState('')
+
+  useEffect(() => {
+    BarCodeScanner.requestPermissionsAsync().then(({ status }) =>
+      setHasPermission(status === 'granted')
+    )
+  }, [])
+
+  const handleBarCodeScanned = ({ data }) => {
+    setCode(data)
+  }
+
+  if (hasPermission === null)
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Text>Requested camera permission</Text>
+      </View>
+    )
+  if (hasPermission === false)
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <Text>No access to camera</Text>
+      </View>
+    )
+
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      {code ? (
+        <Text>{code}</Text>
+      ) : (
+        <BarCodeScanner
+          onBarCodeScanned={handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+          barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+        />
+      )}
+      <Button title="witam" onPress={() => setCode('')} />
+    </View>
+  )
+}
 
 export default Other
